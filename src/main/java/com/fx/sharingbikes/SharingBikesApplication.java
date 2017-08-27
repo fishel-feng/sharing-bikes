09603cddbe04ff907bb334f070e7c961f11c04fb
fx109138 <1091381572@qq.com>
2017-08-27 21:46:27 +0800

    first commit

---
+++ src/main/java/com/fx/sharingbikes/SharingBikesApplication.java
@@ -0,0 +1,12 @@
+package com.fx.sharingbikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingBikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingBikesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fx/sharingbikes/SharingBikesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fx.sharingbikes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SharingBikesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
